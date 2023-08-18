bb4ff525d9631f49d26e878d3e306dd716073550
inayong <141033984+inayong@users.noreply.github.com>
2023-08-18 13:10:36 +0900

    dfs

---
+++ tests/test_dfs.py
@@ -0,0 +1,11 @@
+import unittest
+
+from lib.dfs.ice import ice
+
+class TestDFS(unittest.TestCase):
+    def test_ice(self):    
+        m = [[0,0,1,1,0],
+            [0,0,0,1,1],
+            [1,1,1,1,1],
+            [0,0,0,0,0]]  
+        self.assertEqual(ice(m), 3)
